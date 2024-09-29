b48a1e23737496afe546d7c87423409de9ad757d
Raimond Perminov <raymond@Raimonds-MacBook-Pro.local>
2024-09-29 22:56:45 +0300

    Change the background color. Add the ability to draw more than one figure

--- main.py
+++ main.py
@@ -10,33 +10,52 @@ class PaintWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setMouseTracking(True)
-        self.points = []
+        self.figures = []  # Список для зберігання всіх фігур (ліній)
+        self.current_points = []  # Точки для поточної фігури
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
-            self.points = [event.pos()]  # Початок нової фігури
+            self.current_points = [event.pos()]  # Початок нової фігури
             self.update()
 
     def mouseMoveEvent(self, event):
         if event.buttons() & Qt.MouseButton.LeftButton:
-            self.points.append(event.pos())  # Додаємо нову точку
+            self.current_points.append(event.pos())  # Додаємо нову точку до поточної фігури
             self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.figures.append(self.current_points)  # Додаємо поточну фігуру до списку всіх фігур
+            self.current_points = []
 
     def paintEvent(self, event):
         painter = QPainter(self)
+
+        # Фон для віджета
+        painter.fillRect(self.rect(), Qt.GlobalColor.white)
+
+        # Налаштування пензля для малювання
         pen = QPen(Qt.GlobalColor.black, 2, Qt.PenStyle.SolidLine)
         painter.setPen(pen)
 
-        if len(self.points) > 1:
-            for i in range(len(self.points) - 1):
-                painter.drawLine(self.points[i], self.points[i + 1])
+        # Малюємо всі збережені фігури
+        for points in self.figures:
+            if len(points) > 1:
+                for i in range(len(points) - 1):
+                    painter.drawLine(points[i], points[i + 1])
+
+        # Малюємо поточну фігуру
+        if len(self.current_points) > 1:
+            for i in range(len(self.current_points) - 1):
+                painter.drawLine(self.current_points[i], self.current_points[i + 1])
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        
+
         # Замінюємо звичайний віджет на PaintWidget
         self.paint_widget = PaintWidget(self.ui.centralwidget)
         self.paint_widget.setGeometry(self.ui.widget.geometry())
@@ -45,6 +64,7 @@ class MainWindow(QMainWindow):
         # Видаляємо попередній простий віджет
         self.ui.widget.deleteLater()
         self.ui.widget = self.paint_widget
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
